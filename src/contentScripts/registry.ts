7346c524b03f161229d8883cc6b7543ff0698d64
Tachibana Shin <tachibshin@duck.com>
2026-04-16 14:51:21 +0700

    feat(registry): enhance content extraction logic
    - Improve selector for content blocks
    - Refine target element identification
    - Enhance image attribute handling
    - Improve cover image extraction
    - Make title cleaning

--- src/contentScripts/registry.ts
+++ src/contentScripts/registry.ts
@@ -233,11 +233,16 @@ const registry: SiteConfig[] = [
 
       $("h3:contains(Ghi chú):not(h3:has(+ .mw-references-wrap))").remove()
 
+      $("h2:first-child:has(.mw-headline)").remove()
+
       $("img").each((_, img) => {
         const $img = $(img)
 
         $img.attr("data-src", $img.attr("data-src")?.split("/revision/")[0])
-        $img.attr("src", $img.attr("data-src")?.split("/revision/")[0])
+        $img.attr(
+          "src",
+          ($img.attr("data-src") || $img.attr("src"))?.split("/revision/")[0]
+        )
 
         if ($img.parent().is("a")) {
           $img.parent().replaceWith($img)
@@ -256,15 +261,34 @@ const registry: SiteConfig[] = [
         .at(-1)
         ?.trim()
     },
-    findBlocks: "h3:has(+ dl)",
-    findTarget: (h3) => h3.nextElementSibling! as HTMLElement,
+    findBlocks:
+      "h3:has(+ ul), h3:has(+ figure + ul), h3:has(+ figure + * + ul), h3:has(+ figure + table), h3:has(+ figure + * + table)" +
+      ", .wds-tabber > .wds-tab__content > h3:has(+ .volume)",
+    findTarget: (h3) => {
+      let ul = h3.nextElementSibling
+
+      if (ul?.classList.contains("volume")) return ul as HTMLElement
+
+      if (ul && ul.tagName === "FIGURE") {
+        ul = ul.nextElementSibling
+      }
+
+      while (ul && ul.tagName !== "UL" && ul.tagName !== "TABLE") {
+        ul = ul.nextElementSibling
+      }
+
+      if (ul === null) throw new Error("Can't find target")
+
+      return ul as HTMLElement
+    },
     extractCover: (h3) => {
-      const fig = h3.previousElementSibling
-      if (fig?.tagName === "FIGURE") {
-        return fig
-          .querySelector("img")
-          ?.getAttribute("src")
-          ?.replace(/(width|height)=\d*/gi, "width=800")
+      // 表紙画像の抽出
+      const ul = h3.nextElementSibling
+      if (ul && ul.tagName === "FIGURE") {
+        return ul.querySelector("img")?.getAttribute("src") ?? undefined
+      }
+      if (ul && ul.classList.contains("volume")) {
+        return ul.querySelector("img")?.getAttribute("src") ?? undefined
       }
       return undefined
     },
@@ -277,6 +301,16 @@ const registry: SiteConfig[] = [
     cleaner: ($) => {
       $(".wikitable, .mw-editsection, .printfooter").remove()
       $("#toc + h2, #toc, .mw-parser-output[lang] + h2").remove()
+
+      $("h2:first-child:has(.mw-headline)").remove()
+
+      $("img").each((_, img) => {
+        const $img = $(img)
+
+        if ($img.parent().is("a")) {
+          $img.parent().replaceWith($img)
+        }
+      })
     },
     title: (h3) =>
       Array.from(h3.querySelector(".mw-headline")?.childNodes ?? [])
@@ -285,7 +319,7 @@ const registry: SiteConfig[] = [
         .filter(Boolean)
         .join(" ")
         .trim()
-        .replace(/\(\s*\)/g, "")
+        .replace(/\([\s_\-]*\)/g, "")
         .trim()
   },
   {
@@ -297,7 +331,8 @@ const registry: SiteConfig[] = [
         .join(", ")
     },
     findBlocks: ".module-details .module-header",
-    findTarget: (h3) => h3.closest(".module-details .module-chapters-list")!,
+    findTarget: (h3) =>
+      h3.closest(".module-details")!.querySelector(".module-chapters-list")!,
     extractCover: (h3) =>
       h3
         .closest(".module-content")
