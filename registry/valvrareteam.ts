59645e335e8d9a577eab1eecdc08673486afc0c6
Tachibana Shin <tachibshin@duck.com>
2026-05-31 20:02:59 +0700

    chore(valvrareteam): implement getChapterTitle

--- registry/valvrareteam.ts
+++ registry/valvrareteam.ts
@@ -7,10 +7,13 @@ export default defineRegistry({
       .join(", ")
   },
   findBlocks: ".module-details .module-header",
-  findTarget: (h3) => h3.closest(".module-details")!.querySelector(".module-chapters-list")!,
+  findTarget: (h3) =>
+    h3.closest(".module-details")!.querySelector(".module-chapters-list")!,
   extractCover: (h3) =>
-    h3.closest(".module-content")?.querySelector(".module-cover-image")?.getAttribute("src") ||
-    undefined,
+    h3
+      .closest(".module-content")
+      ?.querySelector(".module-cover-image")
+      ?.getAttribute("src") || undefined,
   publisher: "valvrareteam.net",
   targetQueries: {
     bookTitle: ".rd-novel-title",
@@ -18,5 +21,15 @@ export default defineRegistry({
     container: ".chapter-content"
   },
   title: (h3) => h3.textContent.trim().replace(/\+$/, "").replace(/\+ H/, "H"),
+  getChapterTitle(anchor) {
+    const title: string[] = []
+    for (const node of Array.from(anchor.childNodes)) {
+      if (node instanceof HTMLElement && node.classList.contains("new-tag"))
+        continue
+      title.push(node.textContent?.trim() ?? "")
+    }
+
+    return title.join(" ")
+  },
   lazyDom: true
 })
